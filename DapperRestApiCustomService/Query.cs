d3359097a240445524298306f0894afc9c3d478c
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-31 22:58:10 +0630

    Feat : Add GetAllBlogsQuery

--- DapperRestApiCustomService/Query.cs
+++ DapperRestApiCustomService/Query.cs
@@ -2,5 +2,12 @@
 {
 	public class Query
 	{
+		public static string GetAllBlogsQuery { get; } =
+		@"SELECT [BlogId]
+      ,[BlogTitle]
+      ,[BlogAuthor]
+      ,[BlogContent]
+      ,[DeleteFlag]
+  FROM [dbo].[Tbl_Blog] ORDER BY BlogId DESC";
 	}
 }
