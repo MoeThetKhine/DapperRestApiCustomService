3401000dbe1fd40dd8f5aaeeb779691de96de0c4
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-31 22:54:47 +0630

    Feat : Add Blog Model

---
+++ DapperRestApiCustomService/BlogModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DapperRestApiCustomService
+{
+	public class BlogModel
+	{
+		public long BlogId { get; set; }
+		public string? BlogTitle { get; set; }
+		public string? BlogAuthor { get; set; }
+		public string? BlogContent { get; set; }
+		public bool DeleteFlag { get; set; }
+	}
+}
