4fca71ba275aab4ff1080d016b68458a96f92bad
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-01 01:37:43 +0630

    Feat : Add GetAllBlogs

--- DapperRestApiCustomService/Controllers/DapperController.cs
+++ DapperRestApiCustomService/Controllers/DapperController.cs
@@ -7,5 +7,25 @@ namespace DapperRestApiCustomService.Controllers
 	[ApiController]
 	public class DapperController : ControllerBase
 	{
+		private readonly DapperService _dapperService;
+
+		public DapperController(DapperService dapperService)
+		{
+			_dapperService = dapperService;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetAllBlogs()
+		{
+			try
+			{
+				var blogs = await _dapperService.QueryAsync<BlogModel>(Query.GetAllBlogsQuery);
+				return Ok(blogs);
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, $"Internal server error: {ex.Message}");
+			}
+		}
 	}
 }
