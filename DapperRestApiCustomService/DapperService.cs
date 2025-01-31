4cef24357a200b126e0383287f488c89d9d884e1
Moe Thet Khine <soniapoudel03@gmail.com>
2025-02-01 01:26:47 +0630

    Feat : Add Connection

--- DapperRestApiCustomService/DapperService.cs
+++ DapperRestApiCustomService/DapperService.cs
@@ -1,5 +1,13 @@
-﻿namespace DapperRestApiCustomService;
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace DapperRestApiCustomService;
 
 public class DapperService
 {
+	private readonly string _connectionString = "Data Source=.;Database=DotNetTrainingBatch5;User Id=sa;Password=sasa@123;TrustServerCertificate=True;";
+
+	private IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+
+
 }
