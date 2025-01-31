1b8f65ecb3e705cc29da78e09b816a49e62953a7
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-31 22:58:36 +0630

    Feat : Add CreateBlogQuery

--- DapperRestApiCustomService/Query.cs
+++ DapperRestApiCustomService/Query.cs
@@ -9,5 +9,8 @@
       ,[BlogContent]
       ,[DeleteFlag]
   FROM [dbo].[Tbl_Blog] ORDER BY BlogId DESC";
+
+		public static string CreateBlogQuery { get; } =
+		"INSERT INTO dbo.Tbl_Blog (BlogTitle, BlogAuthor, BlogContent, DeleteFlag) VALUES (@BlogTitle, @BlogAuthor, @BlogContent, @DeleteFlag);";
 	}
 }
