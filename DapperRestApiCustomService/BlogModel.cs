a4602d6cd99e0ea6d82aff0736c09b1d093d7bc3
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-31 22:56:01 +0630

    Format Code

--- DapperRestApiCustomService/BlogModel.cs
+++ DapperRestApiCustomService/BlogModel.cs
@@ -1,11 +1,10 @@
-﻿namespace DapperRestApiCustomService
+﻿namespace DapperRestApiCustomService;
+
+public class BlogModel
 {
-	public class BlogModel
-	{
-		public long BlogId { get; set; }
-		public string? BlogTitle { get; set; }
-		public string? BlogAuthor { get; set; }
-		public string? BlogContent { get; set; }
-		public bool DeleteFlag { get; set; }
-	}
+	public long BlogId { get; set; }
+	public string? BlogTitle { get; set; }
+	public string? BlogAuthor { get; set; }
+	public string? BlogContent { get; set; }
+	public bool DeleteFlag { get; set; }
 }
